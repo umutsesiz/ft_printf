224dd4554ca62b1bcb8e1ccfc406c729e8148b6f
umutsesiz <99561253+umutsesiz@users.noreply.github.com>
2025-01-07 17:34:20 +0300

    Add files via upload

--- ft_printf.h
+++ ft_printf.h
@@ -13,14 +13,14 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-#include <unistd.h>
-#include <stdarg.h>
+# include <unistd.h>
+# include <stdarg.h>
 
-int ft_printf(const char *, ...);
-int ft_format(va_list *args, char c);
-int ft_is_str(char *str);
-int ft_is_int(int x);
-int ft_is_unsigned(unsigned int x);
+int	ft_printf(const char *input, ...);
+int	ft_format(va_list *args, char c);
+int	ft_is_str(char *str);
+int	ft_is_int(int x);
+int	ft_is_unsigned(unsigned int x);
 int	ft_is_hex(unsigned int x, char c);
 int	ft_is_pointer(unsigned long x, int sign);
 
--- ft_printf_func.c
+++ ft_printf_func.c
@@ -12,58 +12,57 @@
 
 #include "ft_printf.h"
 
-int ft_is_str(char *str)
+int	ft_is_str(char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if(!str)
-        return (write(1, "(null)", 6));
-    while(str[i])
-        write(1, &str[i++], 1);
-    return (i);
+	i = 0;
+	if (!str)
+		return (write(1, "(null)", 6));
+	while (str[i])
+		write (1, &str[i++], 1);
+	return (i);
 }
 
-int ft_is_int(int x)
+int	ft_is_int(int x)
 {
-    int res;
-    char digit;
-    
-    res = 0;
-    if (x == 0)
-        return (write(1, "0", 1));
-    if (x == -2147483648)
-        return (write (1, "-2147483648", 11));
-    if (x < 0)
-    {
-        res += write(1, "-", 1);
-        x = -x;
-    }
-    if (x >= 10)
-        res += ft_is_int(x / 10);
-    
-    digit = "0123456789"[x % 10];
-    res += write(1, &digit, 1);
-    return (res);
+	int		res;
+	char	digit;
+
+	res = 0;
+	if (x == 0)
+		return (write(1, "0", 1));
+	if (x == -2147483648)
+		return (write (1, "-2147483648", 11));
+	if (x < 0)
+	{
+		res += write(1, "-", 1);
+		x = -x;
+	}
+	if (x >= 10)
+		res += ft_is_int(x / 10);
+	digit = "0123456789"[x % 10];
+	res += write(1, &digit, 1);
+	return (res);
 }
 
-int ft_is_unsigned(unsigned int x)
+int	ft_is_unsigned(unsigned int x)
 {
-    int res;
-    char digit;
+	int		res;
+	char	digit;
 
-    res = 0;
-    if (x >= 10)
-        res += ft_is_unsigned(x / 10);
-    digit = "0123456789"[x % 10];
-    res += write(1, &digit, 1);
-    return (res);
+	res = 0;
+	if (x >= 10)
+		res += ft_is_unsigned(x / 10);
+	digit = "0123456789"[x % 10];
+	res += write(1, &digit, 1);
+	return (res);
 }
 
 int	ft_is_hex(unsigned int x, char c)
 {
-	int	res;
-	char digit;
+	int		res;
+	char	digit;
 
 	res = 0;
 	if (x >= 16)
@@ -83,8 +82,8 @@ int	ft_is_hex(unsigned int x, char c)
 
 int	ft_is_pointer(unsigned long x, int sign)
 {
-	int	res;
-	char digit;
+	int		res;
+	char	digit;
 
 	res = 0;
 	if (!x)
